db828d2481288bacd3432f8f6e3a4dbae7df0608
Muhammet Emin Şen <m.emin.sen8@gmail.com>
2024-08-26 12:53:41 +0300

    Update

--- Assets/Scripts/Movement.cs
+++ Assets/Scripts/Movement.cs
@@ -4,15 +4,31 @@ using UnityEngine;
 
 public class Movement : MonoBehaviour
 {
-    // Start is called before the first frame update
+    [Header("Values")]
+    public float speed = 5f;
+    public float mouseSensivity = 5f;
+    float currentRotationX = 0f;
+
+
     void Start()
     {
-        
+        Cursor.lockState = CursorLockMode.Locked;
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        float horizontal = Input.GetAxis("Horizontal") * speed * Time.deltaTime;
+        float vertical = Input.GetAxis("Vertical") * speed * Time.deltaTime;
+        transform.Translate(horizontal, 0, vertical);
+
+
+        float mouseX = Input.GetAxis("Mouse X") * mouseSensivity;
+        float mouseY = Input.GetAxis("Mouse Y") * mouseSensivity;
+
+        currentRotationX -= mouseY;
+        currentRotationX = Mathf.Clamp(currentRotationX, -25f, 25f);
+
+        transform.rotation = Quaternion.Euler(0, transform.rotation.eulerAngles.y + mouseX, 0);
+        Camera.main.transform.localRotation = Quaternion.Euler(currentRotationX, 0, 0);
     }
 }
